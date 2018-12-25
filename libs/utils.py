eadb43dcd6bec30e516613dcbcc6e38ecdbc5946
黑と白の印記 <smilingxinyi@gmail.com>
2018-12-26 04:56:55 +0800

    root path & yaml write, read

--- libs/utils.py
+++ libs/utils.py
@@ -3,23 +3,9 @@
 # from .__version__ import __version__ as VERSION
 
 import os, sys
+import yaml
 
 platform = sys.platform
-
-def check_files_exists(files):
-	'Check files exists'
-
-	pass
-
-def get_file_content(filename):
-	'Get file content'
-
-	if check_files_exists(filename):
-		with open(filename) as file:
-			content = file
-			return content
-	else:
-		return False
 
 def which(program):
 	'Get program command path'
@@ -45,3 +31,85 @@ def is_virtual():
 
 	return (hasattr(sys, 'real_prefix') or
             (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+
+def parents(parents_name, current_path = None):
+	'Get parents name path'
+
+	parent_name = os.path.dirname(current_path or os.path.abspath(__file__))
+	dict_name = parent_name.split('/')[-1]
+	if parents_name == dict_name:
+		return parent_name
+	elif dict_name == '/':
+		return None
+	else:
+		return parents(parents_name, parent_name)
+
+def root_path(root_name):
+	'''
+	Append root path
+	:param root_name: append target folder name
+	:return:
+	'''
+
+	rootpath = parents(root_name)
+	sys.path.append(rootpath)
+
+def check_files_exists(files):
+	'Check files exists'
+
+	pass
+
+def get_file_content(filename):
+	'Get file content'
+
+	if check_files_exists(filename):
+		with open(filename) as file:
+			content = file
+			return content
+	else:
+		return False
+
+class classproperty(object):
+	'Class property'
+
+	'''
+	Usage: 
+	
+	class Status(enum.Enum):
+		WAITING = 0,
+		RUNNING = 1
+	'''
+
+	def __init__(self, getter):
+		self.getter= getter
+	def __get__(self, instance, owner):
+		return self.getter(owner)
+
+def write_yaml(file, data):
+	'''
+	Write yaml file
+	:param file: (path) filename
+	:param data: write data
+	:return: None is ok or error message
+	'''
+	try:
+		with open(file, 'wb') as yaml_file:
+			yaml.dump(data, yaml_file, default_flow_style=False)
+			yaml_file.close()
+			return None
+	except OSError as err_msg:
+		print err_msg
+
+def read_yaml(file):
+	'''
+	Read yaml file
+	:param file: (path) filename
+	:return: file data or error message
+	'''
+	try:
+		with open(file) as yaml_file:
+			data = yaml.load(yaml_file)
+			yaml_file.close()
+			return data
+	except OSError as err_msg:
+		return err_msg
