9f1fe4e630fd60a59fd71062347ab1d98bca6cca
黑と白の印記 <smilingxinyi@gmail.com>
2019-07-08 06:33:41 +0800

    v1.0.1

--- release.sh
+++ release.sh
@@ -2,4 +2,7 @@
 rm -rf build && rm -rf dist && rm -rf scv.egg* && rm -rf *.egg-info
 # echo -e "Is the version number upgraded? (y/n)"
 # python setup.py sdist bdist_wheel
-# twine upload dist/*
+# twine upload dist/
+#
+# twine upload -r local --sign -identity user_name ./foo-1.zip
+# twine upload --repository-url https://pypi.misaka.ink/ dist/*
--- utils/__init__.py
+++ utils/__init__.py
@@ -7,4 +7,7 @@ Utils
 Utils is a collection of common Python functions and classes
 """
 
+print '可以吧'
+
+name = 'utils'
 from utils import *
